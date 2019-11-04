5e0ea736833c72c1250995c66e4e92381c38ba43
rwdrafting <44788866+rwdrafting@users.noreply.github.com>
2019-11-04 09:47:48 -0800

    Update InsightCode.py

--- src/InsightCode.py
+++ src/InsightCode.py
@@ -3,18 +3,21 @@ import csv
 import pandas as pd
 
 #Setting up input and output
-Input = 'Border_Crossing_Entry_Data.csv'
+Input = 'C:\\Users\\jessw\\Downloads\\border-crossing-analysis-master\\border-crossing-analysis-master\\input\\Border_Crossing_Entry_Data.csv'
 Output_path = 'C:\\Users\\jessw\\Downloads\\border-crossing-analysis-master\\border-crossing-analysis-master\\output\\Report.csv'
 #reading in data from the specified csv input file
 data = pd.read_csv(Input)
 #cleaning data to drop any possible empty values
 data.dropna(inplace=True)
 
+#sorting data by column values
+data_sorted = data.groupby(["Border","Date","Measure"],as_index=False)
+
 #create new dataframe sorted by index  
 data_new = data_sorted.agg({"Value":'sum'}).sort_index()
 
 #adding on new "Average" column to data frame sorted by index
-data_new['Average'] = data_new['Value'].rolling(window=5, center=False).mean()
+data_new['Average'] = round(data_new['Value'].rolling(window=5, center=False).mean())
 
 #sorting final output by column values
 data_final = data_new.sort_values(["Date","Border","Measure"],ascending=[False,False,False])
